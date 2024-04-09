e3dab23cc4f9de10381166c5f8dfe0a7867719ed
BeautifulRus <blue.sky.2021@bk.ru>
2024-04-09 21:55:30 +0500

    Добавил первые зависимости, раннер, драйвер, и пустую фичу кукумбера

---
+++ src/test/java/Driver.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+
+
+abstract public class Driver {
+
+
+    protected static WebDriver driver;
+
+    protected static WebDriverWait wait;
+
+
+    public void initDriver() throws IOException {
+        System.getProperties().load(ClassLoader.getSystemResourceAsStream("application.properties"));
+            initLocalDriver();
+    }
+
+
+
+    private void initLocalDriver() {
+        if ("firefox".equalsIgnoreCase(System.getProperty("type.browser"))) {
+            driver = new FirefoxDriver();
+        }else
+        if ("chrome".equalsIgnoreCase(System.getProperty("type.browser"))){
+            driver = new ChromeDriver();
+        }else
+        if ("edge".equalsIgnoreCase(System.getProperty("type.browser"))){
+            driver = new EdgeDriver();
+        }else Assertions.fail("I don't work with that browser. Please change the variable |type.browser| from file |application.properties| to the prescribed option in the code");
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(500));
+
+
+
+    }
+
+
+
+
+}
---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,23 @@
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+
+
+import static io.cucumber.core.options.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key =  FILTER_TAGS_PROPERTY_NAME, value = "@all"),
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/scenarios"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "Step"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
+
+
+})
+
+
+
+public class TestRunner {
+}
